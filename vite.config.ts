f2b09e2d71e848c7abb8c94176ce596be2f9588f
thisisrekha <kondurinaga@iitbhilai.ac.in>
2024-05-21 00:38:17 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tutedudefigma/',
+  build: {
+    outDir: 'build', // Specify the output directory
+  },
   server: { watch: { awaitWriteFinish: true } },
   plugins: [react()],
 });
